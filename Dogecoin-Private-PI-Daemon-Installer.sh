9d49ad342f35c23af402933f4503b4e2b2ebcc00
cryptojoe <cryptojoehodler@gmail.com>
2019-01-14 10:15:12 -0500

    Update script

--- Dogecoin-Private-PI-Daemon-Installer.sh
+++ Dogecoin-Private-PI-Daemon-Installer.sh
@@ -4,8 +4,17 @@
 cd ..
 apt-get update
 
+# Reduce GPU memory since we are CLI based
+sed -i '/gpu_mem/d' /boot/config.txt
+echo "gpu_mem=16" >> /boot/config.txt
+
+# Update PI swap from 100 to 1024
+sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1024/' /etc/dphys-swapfile
+sudo /etc/init.d/dphys-swapfile stop
+sudo /etc/init.d/dphys-swapfile start
+
 # Install Dependancies
-apt-get -qq install zip curl \
+apt-get -qq install -y zip curl \
         build-essential \
         libtool \
         autotools-dev \
@@ -15,17 +24,31 @@ apt-get -qq install zip curl \
         bsdmainutils \
         git cmake \
         libboost-all-dev \
-        libdb5.3-dev libdb5.3++-dev \
         libminiupnpc-dev libzmq3-dev \
         libssl1.0-dev libgmp3-dev
 
+# Install Berkeley DB
+wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
+tar -xzf db-4.8.30.NC.tar.gz
+cd db-4.8.30.NC/build_unix/
+../dist/configure --enable-cxx --disable-shared --with-pic --prefix=/usr
+
 # Clone the Dogecoin Private Core repository and compile
-git clone https://github.com/PrivateDOGP/DOGP-Project.git
-cd DOGP-Project
+git clone https://github.com/PrivateDOGP/DOGP-Project.git /usr/local/dogecoinprivate
+cd /usr/local/dogecoinprivate
 ./autogen.sh
-./configure --disable-tests --without-gui --with-incompatible-bdb --with-unsupported-ssl
-make -j$(nproc)
-echo "Build complete. Installing now!"
+./configure --disable-tests --without-gui --with-unsupported-ssl
+
+echo
+echo "Ready to build, please be patient and do not unplug your device at any time."
+echo
+
+make --quiet -j$(nproc)
+
+echo
+echo "Build complete. Installing now..."
+echo
+
 make install
 
 chown -R $(logname): ../DOGP-Project
